740b114818a9c106949ab7aec172de7fe41726be
qcai2002 <qcai2002@users.noreply.github.com>
2014-11-01 17:13:02 -0700

    use object and for loop

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,38 +1,167 @@
 //Build an interactive resume
 
 //SECTION 1 - BIO
-$("#header").append(HTMLheaderName.replace("%data%", "Jenny Coll"));
-$("#header").append(HTMLheaderRole.replace("%data%", "President"));
+var bio = {
+	"name": "Jenny Coll",
+	"role": "President",
+    "welcomeMsg": "Welcome to Jenny Coll's interactive resume!",
+	"contacts": {
+				"mobile": "510 1234 567",
+				"email": "jennycoll@yahoo.com",
+				"github": "jennycoll",
+				"twitter": "@jennycoll",
+				"location": "SF"	
+	},
+	"skills": ["JS", "HTML", "CSS"],
+	"blog": "jennycoll.com",
+	"bioPic": "images/fry.jpg"
+};
 
-$("#header").append(HTMLcontactGeneric.replace("%data%", "x"));
-$("#header").append(HTMLmobile.replace("%data%", "510-1234-567")); 
-$("#header").append(HTMLemail.replace("%data%", "jenny.coll@yahoo.com")); 
-$("#header").append(HTMLtwitter.replace("%data%", "@jennycoll")); 
-$("#header").append(HTMLgithub.replace("%data%", "jennycoll"));
-$("#header").append(HTMLblog.replace("%data%", "jennycoll.com")); 
-$("#header").append(HTMLlocation.replace("%data%", "SF")); 
+function displayBio()
+{
+	$("#header").prepend(HTMLheaderRole.replace("%data%", bio.role));
+	$("#header").prepend(HTMLheaderName.replace("%data%", bio.name));
 
-$("#header").append(HTMLbioPic.replace("%data%", "images/fry.jpg"));  
-$("#header").append(HTMLWelcomeMsg.replace("%data%", "Welcome to my resume page!"));  
+	$("#topContacts").append(HTMLmobile.replace("%data%", bio.contacts.mobile),
+    	                     HTMLemail.replace("%data%", bio.contacts.email),
+    	                     HTMLgithub.replace("%data%", bio.contacts.github),
+        	                 HTMLtwitter.replace("%data%", bio.contacts.twitter), 
+                	         HTMLlocation.replace("%data%", bio.contacts.location)); 
 
-$("#header").append(HTMLskills.replace("%data%", "JS HTML CSS")); 
+	//$("#header").append(HTMLblog.replace("%data%", bio.blog)); 	
+
+	$("#header").append(HTMLbioPic.replace("%data%", bio.bioPic));  
+
+	$("#header").append(HTMLWelcomeMsg.replace("%data%", bio.welcomeMsg));  
+
+	$("#header").append(HTMLskillsStart);
+	$("#skills").append(HTMLskills.replace("%data%", bio.skills)); 
+}
+
+displayBio();
 
 //SECTION 2 - WORK
-$("#header").append(HTMLworkEmployer.replace("%data%", "udacity.com")); 
-$("#header").append(HTMLworkTitle.replace("%data%", "developer")); 
-$("#header").append(HTMLworkDates.replace("%data%", "2100-2102")); 
-$("#header").append(HTMLworkLocation.replace("%data%", "MV")); 
-$("#header").append(HTMLworkDescription.replace("%data%", "design backend")); 
+var work = {
+	"jobs": [
+	{
+		"employer": "The Hearth",
+		"title": "President",
+		"dates": "2012-Present",
+		"location": "MV",
+		"description": "lead"
+	},
+	{
+		"employer": "SF Martial Arts",
+		"title": "Instructor",
+		"dates": "2013-Present",
+		"location": "SF",
+		"description": "teach"		
+	}
+	]
+};
+
+function displayWork() {
+	$("#workExperience").append(HTMLworkStart);
+	for (var item in work.jobs) {
+		job = work.jobs[item];
+		$(".work-entry:last").append(HTMLworkEmployer.replace("%data%", job.employer), 
+			                		HTMLworkTitle.replace("%data%", job.title),  
+		                    		HTMLworkDates.replace("%data%", job.dates),
+		                    		HTMLworkLocation.replace("%data%", job.location),
+		                    		HTMLworkDescription.replace("%data%", job.description)); 
+	}
+}
+
+displayWork();
 
 //SECTION 3 - PROJECTS
-$("#header").append(HTMLprojectTitle.replace("%data%", "jennycoll.com")); 
-$("#header").append(HTMLprojectDates.replace("%data%", "2014")); 
-$("#header").append(HTMLprojectDescription.replace("%data%", "blog")); 
-$("#header").append(HTMLprojectImage.replace("%data%", "images/197x148.gif")); 
+var projects = {
+	"projects": [
+	{
+		"title": "interactive resume",
+		"dates": "2014",
+		"description": "JS",
+		"image": "images/197x148.gif"
+	},
+	{
+		"title": "apple watch",
+		"dates": "2015",
+		"description": "Swift",
+		"image": "images/197x148.gif"		
+	}
+	]
+};
+
+function displayProjects() {
+	$("#projects").append(HTMLprojectStart);
+	for (var item in projects.projects) {
+		project = projects.projects[item];
+		$(".project-entry:last").append(HTMLprojectTitle.replace("%data%", project.title), 
+		               		  HTMLprojectDates.replace("%data%", project.dates),
+		               		  HTMLprojectDescription.replace("%data%", project.description), 
+		               		  HTMLprojectImage.replace("%data%", project.image)); 			
+	}
+}
+
+displayProjects();
+
 
 //SECTION 4 - EDUCATION
-$("#header").append(HTMLschoolName.replace("%data%", "udacity.com")); 
-$("#header").append(HTMLschoolDegree.replace("%data%", "Nanodegree")); 
-$("#header").append(HTMLschoolDates.replace("%data%", "2012-2014")); 
-$("#header").append(HTMLschoolLocation.replace("%data%", "MV")); 
-$("#header").append(HTMLschoolMajor.replace("%data%", "Web Development")); 
+var education = {
+	"schools": [
+	{
+		"name": "University1",
+		"degree": "Master",
+		"dates": "2012-Present",
+		"location": "MV",
+		"major": "Science"
+	},
+	{
+		"name": "University2",
+		"degree": "Master",
+		"dates": "2012-Present",
+		"location": "MV",
+		"major": "Arts"
+	}
+	],
+	"onlineCourses": [
+	{
+		"title": "Web Development",
+		"school": "udacity.com",
+		"dates": "2012-Present",
+		"url": "https://www.udacity.com/course/cs253"	
+	},
+	{
+		"title": "Machine Learning",
+		"school": "coursera.org",
+		"dates": "2012-Present",
+		"url": "https://www.coursera.org/course/ml"	
+	}
+	]
+};
+
+function displayEducation() {
+	$("#education").append(HTMLschoolStart);
+	for (var item in education.schools) {
+		school = education.schools[item];
+		$(".education-entry:last").append(HTMLschoolName.replace("%data%", school.name), 
+		              		   HTMLschoolDegree.replace("%data%", school.degree), 
+		              		   HTMLschoolDates.replace("%data%", school.dates),
+		              		   HTMLschoolLocation.replace("%data%", school.location), 
+		              		   HTMLschoolMajor.replace("%data%", school.major)); 		
+	}
+
+    $(".education-entry:last").append(HTMLonlineClasses);
+	for (var item in education.onlineCourses) {
+		course = education.onlineCourses[item];
+		$(".education-entry:last").append(HTMLonlineTitle.replace("%data%", course.title), 
+		              		   HTMLonlineSchool.replace("%data%", course.school), 
+		              		   HTMLonlineDates.replace("%data%", course.dates),
+		              		   HTMLonlineURL.replace("%data%", school.url));		
+	}	
+}
+
+displayEducation();
+
+$("#main").append(internationalizeButton);
+$("#mapDiv").append(googleMap); 
